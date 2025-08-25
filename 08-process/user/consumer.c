c8ab61c023591b291fca87c29ccb27f700ebfd9d
Delfina Buil <delfinabuil2@gmail.com>
2025-08-25 09:33:11 -0300

    first implementation of consumer and producer processes

--- 08-process/user/consumer.c
+++ 08-process/user/consumer.c
@@ -1,7 +1,33 @@
 #include "edoslib.h"
 
+int empty, full, mutex;       // semaphore descriptors
+
 int main(void)
 {
     printf("Consumer process started with PID %d\n", getpid());
+
+    mutex = semget(1);
+    empty = semget(2);
+    full  = semget(3);
+
+
+    while (true) {
+        sem_wait(&empty);
+        int value = get_from_buffer();
+        sem_signal(&full);
+        process(value);
+    }
+
     return 0;
+}
+
+int get_from_buffer() {
+    sem_wait(&mutex);
+    // extract item from buffer
+    sem_signal(&mutex);
+}
+
+
+void process(int value) {
+     printf("Consumer [%d] processed item: %d\n", getpid(), value);
 }
--- 08-process/user/producer.c
+++ 08-process/user/producer.c
@@ -1,7 +1,31 @@
 #include "edoslib.h"
 
+void add_to_buffer(int value)
+{
+    sys_semwait(&mutex);
+
+    // add value to buffer
+
+    sys_semsignal(&mutex);
+}
+
+static int value_counter = 0;
+int gen_value(void)
+{
+    return value_counter++;
+}
+
 int main(void)
 {
     printf("Producer process started with PID %d\n", getpid());
+
+    while (true)
+    {
+        int value = gen_value();
+        sem_wait(&full);
+        add_to_buffer(value);
+        sem_notify(&empty);
+    }
+
     return 0;
 }
