367e00348a8ec766e4f631ef2ad87afc1fe16952
Delfina Buil <delfinabuil2@gmail.com>
2025-09-02 08:15:01 -0300

    add sempahores to consumer and producer

--- 08-process/user/consumer.c
+++ 08-process/user/consumer.c
@@ -1,7 +1,7 @@
 #include "edoslib.h"
 
-// Shared buffer address
-#define SHARED_BUFFER ((volatile int *)0x40000000)
+// Shared buffer address (mapped differently than producer!)
+#define SHARED_BUFFER ((volatile int *)0x20000000)
 
 int get_from_buffer()
 {
@@ -19,16 +19,26 @@ int main(void)
 {
     printf("Consumer process started with PID %d\n", getpid());
 
+    // Just use the same IDs created by producer
+    // (no need to call semcreate again, just semwait/semsignal)
     while (true)
     {
+        // Wait until buffer has something
+        semwait(2);   // full
+
+        // Enter critical section
+        semwait(3);   // mutex
         int value = get_from_buffer();
-        process(value);
-        
+        //process(value);
+        semsignal(3); // mutex
+
         printf("Consumer read: %d\n", value);
 
-        // Small delay to see the output
-        for (int i = 0; i < 2000000; i++)
-            ;
+        // Signal that buffer is empty again
+        semsignal(1); // empty
+
+        // Small delay
+        for (int i = 0; i < 2000000; i++);
     }
 
     return 0;
--- 08-process/user/producer.c
+++ 08-process/user/producer.c
@@ -1,7 +1,14 @@
 #include "edoslib.h"
 
-// Shared buffer address
+// Shared buffer address (mapped differently than consumer!)
 #define SHARED_BUFFER ((volatile int *)0x40000000)
+
+static int value_counter = 0;
+
+int gen_value(void)
+{
+    return value_counter++;
+}
 
 void add_to_buffer(int value)
 {
@@ -9,26 +16,34 @@ void add_to_buffer(int value)
     *SHARED_BUFFER = value;
 }
 
-static int value_counter = 0;
-int gen_value(void)
-{
-    return value_counter++;
-}
-
 int main(void)
 {
     printf("Producer process started with PID %d\n", getpid());
 
+    // Create semaphores
+    semcreate(1, 1);   // empty semaphore: counts how many slots are available for producer to write (only one slot available in total).
+    semcreate(2,0);   // full semaphore: counts how many slots are filled for consumer to read.
+    semcreate(3,1);   // mutex semaphore: ensures mutual exclusion when accessing the shared buffer.
+
     while (true)
     {
         int value = gen_value();
+
+        // Wait until buffer has space
+        semwait(1);   // empty
+
+        // Enter critical section
+        semwait(3);   // mutex
         add_to_buffer(value);
+        semsignal(3); // mutex
 
         printf("Producer wrote: %d\n", value);
 
-        // Small delay to see the output
-        for (int i = 0; i < 2000000; i++)
-            ;
+        // Signal that buffer is full
+        semsignal(2); // full
+
+        // Small delay
+        for (int i = 0; i < 2000000; i++);
     }
 
     return 0;
